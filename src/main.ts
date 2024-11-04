a276a1a0deeaa83aa6a6ed6bdf4e84478eac7527
Максим Луговской <lugovskou.myu@yandex.ru>
2024-11-05 00:29:43 +0500

    add: helmet & CORS

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import helmet from 'helmet';
 
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  await app.listen(process.env.PORT ?? 3001);
+  app.use(helmet());
   app.setGlobalPrefix('api');
   await app.listen(3001);
 }
